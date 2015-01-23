58405911dfbb820dc5ca5479c9acabaeb70985b8
Jon Borgonia <jon@gomagames.com>
2015-01-23 08:57:56 -1000

    non-working entity.collidePoint()

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,9 +1,47 @@
 import com.haxepunk.Scene;
+import com.haxepunk.utils.Input;
 
 class MainScene extends Scene
 {
+
+  private var left_button:Button;
+  private var right_button:Button;
+
 	public override function begin()
 	{
+    /*
+      Create two buttons for testing
+     */
+    left_button = new Button();
+    right_button = new Button();
+
+
+    /*
+      Add the buttons to this scene
+      http://haxepunk.com/documentation/api/com/haxepunk/Scene.html#add
+     */
+    add( left_button );
+    add( right_button );
+
+    // position the buttons
+    left_button.x = 150;
+    left_button.y = 150;
+
+    right_button.x = 350;
+    right_button.y = 150;
 
 	}
+
+  public override function update():Void
+  {
+    if(Input.mouseDown){
+      trace( Input.mouseX, Input.mouseY );
+      if( left_button.collidePoint( left_button.x, left_button.y, Input.mouseX, Input.mouseY ) ){
+        left_button.activate();
+      }
+    }else if(Input.mouseReleased){
+      left_button.deactivate();
+    }
+  }
+
 }
